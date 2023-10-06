d497b4f98a0d301592dab0fdee09872543f437ce
julilan <juli.molnar@outlook.com>
2023-10-06 10:27:03 +0300

    Prettier reformat

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-App will be here
-    </div>
-  );
+  return <div className='App'>App will be here</div>;
 }
 
 export default App;
