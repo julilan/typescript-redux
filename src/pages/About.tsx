395aa2042a9813d256d309f00a454d5d5c59c99a
julilan <juli.molnar@outlook.com>
2023-10-06 13:29:33 +0300

    new pages

---
+++ src/pages/About.tsx
@@ -0,0 +1,23 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores,
+        labore? Facilis velit obcaecati eveniet libero possimus recusandae
+        doloribus, expedita sed! Dolorum enim quia quaerat, harum impedit
+        incidunt minus quidem eos consectetur, laudantium tempora consequuntur
+        quibusdam itaque, iure magnam blanditiis! Sunt corrupti, nesciunt alias
+        accusantium ratione, ad atque voluptates perferendis exercitationem ex,
+        quod magni beatae ipsa quis ipsum quo. Fugiat odit aliquam aspernatur
+        mollitia cupiditate eius ex reprehenderit id, consequuntur incidunt iure
+        doloribus possimus, qui nesciunt veritatis quas? Quaerat eveniet minus
+        commodi non impedit tempore repellendus provident? Tenetur quisquam sunt
+        dolorem laborum assumenda cumque nobis ipsa, ducimus unde iste suscipit
+        laboriosam?
+      </p>
+    </div>
+  );
+};
+
+export default About;
